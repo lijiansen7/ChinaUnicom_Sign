5ea7d08bea17875c7f0459ca6d215d45346d00c0
lijiansen7 <74719704+lijiansen7@users.noreply.github.com>
2022-02-14 21:06:49 +0800

    Update ChinaUnicom_sing.py

--- ChinaUnicom_sing.py
+++ ChinaUnicom_sing.py
@@ -1,8 +1,13 @@
+import random
 import requests
+
+cookie_list = ['cookei1','cookie2']#这里填联通营业厅cookie,几天就可能失效了，暂时不会写服务密码登录
 
 def sign(cookie):
     sign_url = 'http://act.10010.com/SigninApp/signin/daySign'
+    ip = random.randint(15,26)
     headers = {
+        'X-Forword-ip' : f'58.142.78.{ip}',
         'Host': 'act.10010.com',
         'Connection': 'keep-alive',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 11; MI 10s Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36;',
@@ -10,22 +15,9 @@ def sign(cookie):
         'Cookie': cookie
         }
     data = {}
-    sign = requests.post(url=sign_url,headers=headers,data=data).text
+    sign = requests.post(url=sign_url,headers=headers,data=data).json()
+    print(ip)
     return sign
 
-def Task(key,qq_num,cookie):
-    i=0
-    while i<2:
-        sign_2 = sign(cookie)
-        if '0002' in sign_2:
-            qmsg = f'https://qmsg.zendee.cn/send/{key}?msg=您今天已经签到啦&qq_num={qq_num}'
-            requests.get(url=qmsg)
-            i+=2
-            print(sign_2)
-        else:
-            i+=1
-            qmsg = f'https://qmsg.zendee.cn/send/{key}?msg=签到失败&qq_num={qq_num}'
-            requests.get(url=qmsg)
-
 for cookie in cookie_list:
-    Task(key,qq_num,cookie)
+    print(sign(cookie)['msg'])
